12bb3469b1c2b689aa491e61f45162e9ff891b73
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2016-08-28 21:26:25 +0200

    Initial commit

---
+++ Assets/SCRIPTS/focusScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class focusScript : MonoBehaviour {
+	public AudioMixerSnapshot normal;
+	public AudioMixerSnapshot focus;
+
+	public float transitionTime;
+
+	// Use this for initialization
+	void Start() {
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		if(Input.GetKeyDown(KeyCode.E)) {
+			focus.TransitionTo(transitionTime);
+		} else if(Input.GetKeyUp(KeyCode.E)) {
+			normal.TransitionTo(transitionTime);
+		}
+	
+	}
+
+	void Focus() {
+		focus.TransitionTo(.5f);
+	}
+}
+
